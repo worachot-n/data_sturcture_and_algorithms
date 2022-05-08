4f46d572730b4766fb9aaa37999b794cd70c4aba
ball-wch <worachotn@gmail.com>
2022-05-08 14:51:16 +0700

    add new function

--- 1.1.2_GPA_calculate.py
+++ 1.1.2_GPA_calculate.py
@@ -17,3 +17,17 @@ while not done:
         total_points += points[grade]
 if num_courses > 0:
     print('Your GPA is {0:.3}'.format(total_points / num_courses))
+
+
+#Default Parameter Value
+def compute_gpa(grade, points={'A+': 4.0, 'A': 4.0, 'A-': 3.67, 'B+': 3.33, 'B':3.0, 'B-': 2.67, 
+        'C+': 2.33, 'C': 2.0, 'C-': 1.67, 'D+': 1.33, 'D': 1.0, 'F': 0}):
+    num_courses = 0
+    total_points = 0
+    for g in grade:
+        num_courses += 1
+        total_points += points[g]
+    return total_points/num_courses
+
+if __name__ == '__main__':
+    print(f"Your GPA is {compute_gpa(['A', 'B', 'C', 'D', 'F'])}")
