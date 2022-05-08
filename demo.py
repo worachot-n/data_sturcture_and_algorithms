0f0cb2733022e5d80b60261dce5070595cc108d4
ball-wch <worachotn@gmail.com>
2022-05-08 15:09:22 +0700

    try demo

--- demo.py
+++ demo.py
@@ -1,2 +1,14 @@
+# n = -10
+# if n >= 0:
+#     param = n
+# else:
+#     param = -n 
+# print(param)
 
+# n = -10
+# params = n if n >= 0 else -n 
+# print(params)
 
+# n = 10
+# factors = {k: k for k in range(1,n+1) if n % k == 0}
+# print(factors)
